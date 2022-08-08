fb9193848eea7d654d9dfc1d13209aeaa72d0689
Mateus Nunes <96783028+mateussuricato@users.noreply.github.com>
2022-08-08 12:19:41 -0300

    feature: changed the assets to the right ones

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,13 +1,12 @@
 import * as S from "./styles"
 import Logo from "../../assets/logo_patterns/logo.png";
-import HomeIcon from "../../assets/icons/home.svg"
 
 const Menu = () => {
   return (
     <S.MenuContainer>
     <img src={Logo} alt="logo" />
     <div>
-      <img src={HomeIcon} alt="home" />
+      <img src='' alt="home" />
       <img src="" alt="settings" />
     </div>
     <img src="" alt="logout" />
