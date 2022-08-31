b18abc918e8b94721acd43eafc0db2dd927e3642
Mateus Nunes <96783028+mateussuricato@users.noreply.github.com>
2022-08-30 23:59:16 -0300

    Update index.tsx

--- src/services/index.tsx
+++ src/services/index.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "projetoblue-hamburgueria-api-production.up.railway.app",
+  baseURL: "https://projetoblue-hamburgueria-api-production.up.railway.app",
 });
